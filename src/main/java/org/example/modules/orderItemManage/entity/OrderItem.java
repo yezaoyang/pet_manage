f651065be568bac904579f61336730d6a9178290
yinxiuhui <2267842122@qq.com>
2026-04-02 10:19:02 +0800

    增加的pojo类

---
+++ src/main/java/org/example/modules/orderItemManage/entity/OrderItem.java
@@ -0,0 +1,61 @@
+package org.example.modules.orderItemManage.entity;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+    private Long id;
+    private Long orderId;
+    private Long goodId;
+    private String goodName;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(Long goodId) {
+        this.goodId = goodId;
+    }
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public void setGoodName(String goodName) {
+        this.goodName = goodName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    // getter setter
+}
